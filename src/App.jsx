f2374533c4b8ec2ca9b80d87b81640339fbb8097
Andrew DGalga <ex.spake@gmail.com>
2024-05-10 20:56:40 -0500

    going to try text area.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,13 @@ import './style.css'
 
 function App() {
 
-  return (
-    <></>
-  )
+    return (
+        <main>
+            <form className="cmd">
+                <textarea className="cmd__line"></textarea>
+            </form>
+        </main>
+    )
 }
 
 export default App
