0c776b8d9356fe2d9bb84253ee14c8d75fa7994e
Andrew DGalga <ex.spake@gmail.com>
2024-05-16 04:39:27 -0500

    base command process.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import './style.css'
+import { createElement } from 'react';
+import './style.css';
+
 
 function App() {
 
+    
+
+    const userCommand = (e) => {
+        e.preventDefault();
+        const cmdout = document.getElementById('cmd__out');
+        const command = e.target.cmd__line.value.toLowerCase();
+        
+        switch(command){
+            default:
+                cmdout.innerText = 'Command not recognized. Try "help".';
+        }
+    }
+
     return (
         <main>
-            <form className="cmd">
+            <div id="cmd__out"></div>
+            <form className="cmd__in" onSubmit={userCommand}>
                 <label htmlFor="cmd__line">andrewjonhard.com&gt;</label>
                 <input id="cmd__line" />
             </form>
