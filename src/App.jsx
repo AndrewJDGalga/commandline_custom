2878f185cdf565d00a98eeccc250e718a26a3649
Andrew DGalga <ex.spake@gmail.com>
2024-05-16 20:44:44 -0500

    decided on reducer. simplest version setup, but I think I want a clunkier version for more detail and future sanity.

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import { createElement } from 'react';
+import { useReducer } from 'react';
 import './style.css';
 
-const acceptedCommands = ['h', ]
+const commandReducer = (state, action) => {
+    switch(action.type){
+        case 'email':
+            return [...state, 'contact@andrewjonhardt.com'];
+        default:
+            return [...state, 'Unknown command, try "help".'];
+    }
+}
 
 function App() {
-
+    const [output, dispatch] = useReducer(commandReducer, '');
     
 
     const userCommand = (e) => {
         e.preventDefault();
-        const cmdout = document.getElementById('cmd__out');
-        const command = e.target.cmd__line.value.toLowerCase();
-        
-        switch(command){
-            default:
-                cmdout.innerText = 'Command not recognized. Try "help".';
-        }
+        const command = e.nativeEvent.submitter.value.toLowerCase()[0] ?? '';
+
+        dispatch({type:command});
     }
 
     return (
         <main>
-            <div id="cmd__out"></div>
-            <form className="cmd__in__mobile">
+            {output}
+            <form className="cmd__in__mobile" onSubmit={userCommand}>
                 <button type="submit" value="portfolio">Portfolio</button>
                 <button type="submit" value="about">About</button>
-                <button type="submit" value="contact">Contact</button>
+                <button type="submit" value="email">Contact</button>
             </form>
             <form className="cmd__in" onSubmit={userCommand}>
                 <label htmlFor="cmd__line">andrewjonhard.com&gt;</label>
